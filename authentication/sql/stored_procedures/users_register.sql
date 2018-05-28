e1e470f4ed6e9316a5d61728136b5182add60497
Allan Chua <allanchua.officefiles@gmail.com>
2018-05-28 22:48:17 +0800

    -Created stored procedure for registering new users to the system.

---
+++ authentication/sql/stored_procedures/users_register.sql
@@ -0,0 +1,30 @@
+CREATE FUNCTION user_register
+(
+	 p_userId UUID
+	,p_firstName VARCHAR(64)
+	,p_lastName VARCHAR(64)
+	,p_email VARCHAR(128)
+	,p_accessKey VARCHAR(1024)
+)
+RETURNS VOID AS
+$BODY$
+	BEGIN
+		INSERT INTO public.users
+		(
+			 userId
+			,firstName
+			,lastName
+			,email
+			,accessKey
+		)
+		VALUES
+		(
+			 p_userId
+			,p_firstName
+			,p_lastName
+			,p_email
+			,p_accessKey
+		);
+	END
+$BODY$
+LANGUAGE 'plpgsql';
