0cd30cd4e9aa77ea0f88ff86e052524b575a396d
Allan Chua <allanchua.officefiles@gmail.com>
2018-06-06 22:24:24 +0800

    -Implemented register user command business service

---
+++ authentication/api/business-layer/user/commands/register.js
@@ -0,0 +1,32 @@
+const registerCommand = require('../../../data-access/user/commands/register');
+const guidFactory = require('../../../utilities/encryption/guid-factory');
+const passwordHasher = require('../../../utilities/encryption/hasher');
+const PASSWORD_SALT = 'Demeter Rocks';
+
+/**
+ * Method used for registering users.
+ * 
+ * @param {object} params - The parameters that would be passed to the query.
+ * @param {string} [params.firstName] - The first name of the user.
+ * @param {string} [params.lastName] - The last name of the user.
+ * @param {string} [params.email] - The email of the user.
+ * @param {string} [params.accessKey] - The access key of the user.
+ * 
+ */
+let execute = function(params) {
+    const input = params || {};
+    const commandParams = {
+        userId: guidFactory.execute(),
+        firstName: params.firstName,
+        lastName: params.lastName,
+        email: params.email,
+        accessKey: passwordHasher.execute(params.accessKey, PASSWORD_SALT),
+        p_isActive: true
+    };
+
+    return registerCommand.execute(commandParams);
+}
+
+module.exports = {
+    execute
+};
