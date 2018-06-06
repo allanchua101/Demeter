aa66bebcb8984ada2ec06172648c703d3b703ddc
Allan Chua <allanchua.officefiles@gmail.com>
2018-06-06 22:09:20 +0800

    -Created a module that would generate guid strings

---
+++ authentication/api/utilities/encryption/guid-factory.js
@@ -0,0 +1,17 @@
+/**
+ * Method used for building a guid string.
+ * 
+ * @returns [string] - A guid in string format.
+ */
+function execute() {
+    function s4() {
+        return Math.floor((1 + Math.random()) * 0x10000)
+            .toString(16)
+            .substring(1);
+    }
+    return s4() + s4() + '-' + s4() + '-' + s4() + '-' + s4() + '-' + s4() + s4() + s4();
+}
+
+module.exports = {
+    execute
+};
